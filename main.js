081ccb69dd36bcd8dace43370e3b001aeb0a6b02
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-02-17 10:03:08 -0500

    Get it working with UNAM tiles

--- main.js
+++ main.js
@@ -1,62 +1,52 @@
-//MIERUNE Color
-const m_color = new L.tileLayer("https://tile.mierune.co.jp/mierune/{z}/{x}/{y}.png", {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
-});
-
-//MIERUNE MONO
-const m_mono = new L.tileLayer("https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png", {
-    attribution: "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
-});
-
 //OSM
-const o_std = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-});
+const osm = new L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
-//GSI Pale
-const t_pale = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
-    attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
-});
-
-//GSI Ort
-const t_ort = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg', {
-    attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
-});
+//UNAM 1965
+const unam1965 = new L.tileLayer(
+  "http://lincolnmullen.com/projects/unam1965/{z}/{x}/{-y}.png",
+  {
+    attribution: "Photograph attribution goes here",
+  }
+);
 
 //MAP
-const map = L.map('map', {
-    center: [35.6831925, 139.7511307],
-    zoom: 13,
-    zoomControl: true,
-    layers: [m_mono]
+const map = L.map("map", {
+  center: [19.326, -99.187],
+  zoom: 13,
+  zoomControl: true,
+  minZoom: 12,
+  maxZoom: 16,
+  layers: [osm, unam1965],
 });
 
-//BaseLayer
+//Base layer
 const Map_BaseLayer = {
-    "MIERUNE Color": m_color,
-    "MIERUNE MONO": m_mono
+  "Open Street Maps": osm,
 };
 
-//AddLayer
+//Additional layers
 const Map_AddLayer = {
-    "OSM": o_std,
-    "GSI Pale": t_pale,
-    "GSI Ort": t_ort
+  "UNAM 1965": unam1965,
 };
 
 //LayerControl
-L.control.layers(
-    Map_BaseLayer,
-    Map_AddLayer,
-    {
-    collapsed: false
-    }
-).addTo(map);
+// Replace `Map_BaseLayer` in the call below with `null` to remove the
+// radio switch for the base layer if desired.
+L.control
+  .layers(Map_BaseLayer, Map_AddLayer, {
+    collapsed: false,
+  })
+  .addTo(map);
 
 //OpacityControl
-L.control.opacity(
-    Map_AddLayer,
-    {
-    label: "Layers Opacity"
-    }
-).addTo(map);
+L.control
+  .opacity(Map_AddLayer, {
+    label: "Aerial photos opacity",
+  })
+  .addTo(map);
